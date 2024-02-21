2b48041b6a9ba5c17947fe1814c2bd795758ed5c
Moustapha406 <mamadoumoustaphadiallo05@gmail.com>
2024-02-21 10:18:52 +0000

    formatage de number au niveau de la vue index production

--- resources/views/TRG/productionJour/index.blade.php
+++ resources/views/TRG/productionJour/index.blade.php
@@ -149,9 +149,9 @@ input[type=number] {
                             @endphp
                             <td> {{ $production['dateFormated'] }} </td>
                             <td> {{ $production['atelier'] }} </td>
-                            <td> {{ $production['quantite'] > 0 ? number_format($production['quantite'],3) : $production['quantite'] }} </td>
+                            <td> {{ $production['quantite'] > 0 ? number_format($production['quantite'],3,',',' ') : $production['quantite'] }} </td>
                             <td> {{ $production['TRG'] }} %</td>
-                            <td> {{$production['cadenceJournaliere']}} </td>
+                            <td> {{number_format($production['cadenceJournaliere'],2,',',' ')}} </td>
                             <td> {{$production['NbreQuartDefault']}} </td>
                             <td class="text-center">
                                 @can('prod-edit')
