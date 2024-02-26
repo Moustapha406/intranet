3952a859d7e6ae5b6577f612d8599683c54e5251
Moustapha406 <mamadoumoustaphadiallo05@gmail.com>
2024-02-26 12:10:47 +0000

    ajouter de la traduction du language en fr

---
+++ lang/fr/passwords.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Votre mot de passe est incorrect.',
+    'sent' => 'We have emailed your password reset link.',
+    'throttled' => 'Veuillez patienter quelques secondes avant de réessayer.',
+    'token' => 'This password reset token is invalid.',
+    'user' => "We can't find a user with that email address.",
+    'failed' => "Votre mot de passe est incorrect."
+
+];
